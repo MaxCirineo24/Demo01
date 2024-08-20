7d19926ab3e4a1ce0f9956011145f87ce0b9ae67
Alumno <Alumno@LAB1507-16>
2024-08-20 17:27:00 -0500

    Agregar archivos de proyecto.

---
+++ Demo01/Persona1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01
+{
+    internal class Persona1
+    {
+        public Persona1(string nombres, string apellidos)
+        {
+            Nombres = nombres;
+            Apellidos = apellidos;
+        }
+
+        public string Nombres { get; set; }
+
+        public string Apellidos { get; set;}
+
+        public string NombresCompletos { get; set; }
+
+        public void AsignarNombresCompletos()
+
+        {
+            NombresCompletos = Nombres + " " + Apellidos;
+        }
+    }
+}
---
+++ Demo01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Demo01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int area = 0;
+            int perimetro = 0;
+            Rectangulo rectangulo = new Rectangulo();
+            rectangulo.Base = 10;
+            rectangulo.Altura = 20;
+
+            area = rectangulo.CalcularArea();
+            perimetro = rectangulo.CalcularPerimetro();
+
+            Console.WriteLine("El area es:");
+            Console.WriteLine(area);
+            Console.WriteLine("El perimetro es:");
+            Console.WriteLine(perimetro);
+
+            Persona1 persona1 = new Persona1
+            {
+                Nombres = "Max",
+                Apellidos = "Cirineo",
+            };
+            persona1.AsignarNombresCompletos();
+            Console.WriteLine(persona1.NombresCompletos);
+
+            Console.WriteLine("Hola Mundo");
+            Console.Read();
+        }
+    }
+}
